206f7f0353515ef4f761c2b85dc42fc500043b5e
SEONGEUN, KIM <manuma159159@gmail.com>
2024-02-06 17:59:32 +0900

    models -jumun-

---
+++ app/models/jumun.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+from sqlalchemy import Column, Integer, String,  DateTime
+from sqlalchemy.orm import DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    pass
+
+class Jumun(Base):
+    __tablename__ = 'jumun'
+
+    jmno = Column(Integer,primary_key=True, autoincrement=True)
+    userid = Column(String(18), nullable=False, unique=True)
+    userloca = Column(String(50), nullable=False)
+    prno = Column(Integer, nullable=False)
+    quantity = Column(Integer,nullable=False, unique=True)
+    jumundate = Column(DateTime, default=datetime.now )
