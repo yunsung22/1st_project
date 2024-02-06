be7c4744b61e86e57495bd8450c24a38bd588002
Seungjae, Seo <seoseungjae20209@gmail.com>
2024-02-06 16:55:27 +0900

    orm 객체 설정 및 db_starup 추가 - jepum 테이블

---
+++ app/models/jepum.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+from sqlalchemy import Column, Integer, String, DateTime, Float
+from sqlalchemy.orm import DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    pass
+
+class Member(Base):
+    __tablename__ = 'product'
+
+    prdno = Column(Integer, primary_key=True, autoincrement=True)
+    prdname = Column(String(18), nullable=False, unique=True)
+    category = Column(DateTime, default=datetime.now)
+    img = Column(String(100), unique=True)
+    stack = Column(Integer, nullable=False)
+    price = Column(Integer, nullable=False)
+    salepoint = Column(Float, nullable=False, unique=True)
