349e2f3908c886d25c370c3143564e7bc312d6b9
Yunsung Lim <kwngjin@gmail.com>
2024-02-06 17:35:11 +0900

    member 테이블 변경

--- app/models/member.py
+++ app/models/member.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy import Column, Integer, String, Float, DateTime, func, Sequence, Date, DECIMAL
+from sqlalchemy import Column, Integer, String, DateTime
 
 class Base(DeclarativeBase):
     pass
@@ -9,9 +9,13 @@ class Base(DeclarativeBase):
 class Member(Base):
     __tablename__ = 'member'
 
-    mno = Column(Integer, primary_key=True, autoincrement=True)
-    userid = Column(String(18), nullable=False, unique=True)
-    passwd = Column(String(18), nullable=False)
-    name = Column(String(10), nullable=False)
-    email = Column(String(50), nullable=False)
-    regdate = Column(DateTime, default=datetime.now(), nullable=True)
+    mno = Column(Integer, primary_key=True, autoincrement=True) #회원번호
+    userid = Column(String(50), nullable=False, unique=True) #고객아이디
+    passwd = Column(String(50), nullable=False) #비밀번호
+    name = Column(String(20), nullable=False)   #고객이름
+    email = Column(String(50), nullable=False)  #이메일
+    addr = Column(String(1000), nullable=False)  #회원주소
+    birth = Column(String(10), nullable=False)  #생년월일
+    phone = Column(String(11), nullable=False)  #핸드폰
+    point = Column(Integer, default=0)  #적립금
+    regdate = Column(DateTime, default=datetime.now(), nullable=True) #회원등록일
