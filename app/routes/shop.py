0aad572fd8559fd5bcf52dffe086e4135d2f23f9
Seokhyeon Kim <tjrgusdlrk101@gmail.com>
2024-02-20 15:48:13 +0900

    코드 중복으로 관리자 페이지 안들어가지는 오류 수정/ 사용하지 않는 페이지 삭제

--- app/routes/shop.py
+++ app/routes/shop.py
@@ -14,25 +14,25 @@ shop_router.mount('/static', StaticFiles(directory='views/static'), name='static
 # oxfords 들어가는 경로
 @shop_router.get('/oxfords', response_class=HTMLResponse)
 def oxfords(req: Request):
-    pdlist = ProductService.select_product()
+    pdlist = ProductService.select2_product()
     return templates.TemplateResponse('main_header/shop/oxfords.html', {'request': req, 'pdlist': pdlist})
 
 # knitwear 들어가는 경로
 @shop_router.get('/knitwear', response_class=HTMLResponse)
 def knitwear(req: Request):
-    pdlist = ProductService.select_product()
+    pdlist = ProductService.select2_product()
     return templates.TemplateResponse('main_header/shop/knitwear.html', {'request': req, 'pdlist': pdlist})
 
 # checkered 들어가는 경로
 @shop_router.get('/checkered', response_class=HTMLResponse)
 def checkered(req: Request):
-    pdlist = ProductService.select_product()
+    pdlist = ProductService.select2_product()
     return templates.TemplateResponse('main_header/shop/checkered.html', {'request': req, 'pdlist': pdlist})
 
 # carryover 들어가는 경로
 @shop_router.get('/carryover', response_class=HTMLResponse)
 def carryover(req: Request):
-    pdlist = ProductService.select_product()
+    pdlist = ProductService.select2_product()
     return templates.TemplateResponse('main_header/shop/carryover.html', {'request': req, 'pdlist': pdlist})
 
 
