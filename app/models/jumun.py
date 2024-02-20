fa0a677056d4740f024429797dc87dbebaaa42d4
SEONGEUN, KIM <manuma159159@gmail.com>
2024-02-20 11:09:23 +0900

    주문

--- app/models/jumun.py
+++ app/models/jumun.py
@@ -14,24 +14,15 @@ class Jumun(Base):
     __tablename__ = 'jumun'
 
     jmno = Column(Integer, primary_key=True, autoincrement=True)  # 주문번호
-    userid = Column(Integer,nullable=False)  # 회원아이디 가져올것
-    addr = Column(String(500), nullable=False)  # 회원 주소 가져올것
-    prno = Column(Integer, nullable=False)  # 제품 번호 가져올것
-    price = Column(Integer, nullable=False)  # 가격
-    prdname = Column(String(50), nullable=False)  # 제품 이름 가져올것
-    stack = Column(Integer, nullable=False)  # 수량 가져올것
-    jumundate = Column(DateTime, default=datetime.now)  # 주문 날짜
-    email = Column(String(50), nullable=False)  # 이메일 가져올것
-    name = Column(String(50), nullable=False)  # 이름 가져올것
-    postalcode = Column(Integer, nullable=False)  # 우편번호 가져올것
-    phone = Column(Integer, nullable=False)  # 핸드폰 번호 가져올것
+    userno = Column(Integer,nullable=False)  # 회원아이디 (member 연동)
+    jpno = Column(Integer, nullable=False)  # 회원 주소 (member 연동)
+    size = Column(String(50), nullable=False)  # 가격 (product 연동)
+    price = Column(Integer, nullable=False)  # 제품 번호 (product 연동)
+    stack = Column(Integer, nullable=False)
+    postcode = Column(Integer, nullable=False)
+    addr = Column(String(100), nullable=False)
+    phone = Column(Integer, nullable=False)
+    regdate = Column(DateTime, default=datetime.now(), nullable=True)
 
 
-class JumunAttach(Base):
-    __tablename__ = 'jumunattach'
-
-    jmano = Column(Integer, primary_key=True, autoincrement=True)
-    jmno = mapped_column(Integer, ForeignKey('jumun.jmno'))
-    fname = Column(String(50), nullable=False)
-    fsize = Column(Integer, default=0)
 
--- app/services/jumun.py
+++ app/services/jumun.py
@@ -1,6 +1,6 @@
 import os
 from sqlalchemy import insert, select
-from app.models.jumun import Jumun, JumunAttach
+from app.models.jumun import Jumun
 from app.dbfactory import Session
 from sqlalchemy import select
 from app.services.product import UPLOAD_DIR
@@ -20,27 +20,21 @@ class JumunService:
                 'phone':jm.phone}
         return data
 
+
     @staticmethod
-    def insert_gallery(jmdto,fname,fsize):
-        # 변환된 이미지 정보를 gallery 테이블에 저장
-        data = JumunService.jumun_convert(jmdto)
-
+    def select_jumun():
         with Session() as sess:
-            stmt = insert(Jumun).values(data)
+            stmt = select(Jumun.jmno, Jumun.userno, Jumun.jpno, Jumun.size,
+                          Jumun.price, Jumun.stack, Jumun.postcode, Jumun.addr
+                          , Jumun.phone) \
+                .join_from(Jumun) \
+                .order_by(Jumun.jmno.desc()).offset(0).limit(10)
             result = sess.execute(stmt)
             sess.commit()
+            return result
 
-            data = {'fname': fname, 'fsize': fsize,
-                    'gno' : result.inserted_primary_key[0]}
-            print(result.inserted_primary_key)
-            stmt = insert(JumunAttach).values(data)
-            result = sess.execute(stmt)
-            sess.commit()
 
-        return result
 
-    def select_jumun(jmdto):
-        data = JumunService
 
 
 
