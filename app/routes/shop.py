d4d5c4001e0c56e966047a8c1d6af127d0c32057
Seungjae, Seo <seoseungjae20209@gmail.com>
2024-02-22 10:24:30 +0900

    shop 페이지 - category에 따라 동적으로 구성

--- app/routes/shop.py
+++ app/routes/shop.py
@@ -5,6 +5,7 @@ from fastapi.staticfiles import StaticFiles
 from starlette import status
 from starlette.responses import RedirectResponse
 
+from app.services.cart import CartService
 from app.services.product import ProductService
 
 shop_router = APIRouter()
@@ -13,30 +14,12 @@ shop_router = APIRouter()
 templates = Jinja2Templates(directory='views/templates')
 shop_router.mount('/static', StaticFiles(directory='views/static'), name='static')
 
+
 # oxfords 들어가는 경로
-@shop_router.get('/oxfords', response_class=HTMLResponse)
-def oxfords(req: Request):
-    pdlist = ProductService.select2_product()
-    return templates.TemplateResponse('main_header/shop/oxfords.html', {'request': req, 'pdlist': pdlist})
-
-# knitwear 들어가는 경로
-@shop_router.get('/knitwear', response_class=HTMLResponse)
-def knitwear(req: Request):
-    pdlist = ProductService.select2_product()
-    return templates.TemplateResponse('main_header/shop/knitwear.html', {'request': req, 'pdlist': pdlist})
-
-# checkered 들어가는 경로
-@shop_router.get('/checkered', response_class=HTMLResponse)
-def checkered(req: Request):
-    pdlist = ProductService.select2_product()
-    return templates.TemplateResponse('main_header/shop/checkered.html', {'request': req, 'pdlist': pdlist})
-
-# carryover 들어가는 경로
-@shop_router.get('/carryover', response_class=HTMLResponse)
-def carryover(req: Request):
-    pdlist = ProductService.select2_product()
-    return templates.TemplateResponse('main_header/shop/carryover.html', {'request': req, 'pdlist': pdlist})
-
+@shop_router.get('/{category}', response_class=HTMLResponse)
+def shop_category(req: Request, category: str):
+    pdlist = ProductService.select2_product(category)
+    return templates.TemplateResponse('main_header/shop/item_gallery.html', {'request': req, 'pdlist': pdlist, 'category': category})
 
 
 @shop_router.get('/itemview/{prdno}', response_class=HTMLResponse)
